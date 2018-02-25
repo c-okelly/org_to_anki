6cebe8cf352494d29c5089cfffebaaba8e6c9af2
c-okelly <okellyconor@gmail.com>
2018-02-25 15:26:12 +0000

    Create console scripts

--- org_to_anki/main.py
+++ org_to_anki/main.py
@@ -2,9 +2,15 @@
 
 # Script to parse differnet formated org files and upload them to anki
 
+def main():
+	print("starting")
+
 def parse_and_upload_org_file(orgFile):
 
 	print("Start prase of org file")
+
+def test():
+	print("starting")
 
 if __name__ == "__main__":
 	print("Start")
--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='org_to_anki',
-		version='0.1',
-		description='Org to Anki notes parser',
-		author='Conor OKelly',
-		author_email='okellyconor@gmail.com',
-		url='https://github.com/c-okelly/org_to_anki',
-		scripts=['bin/joke'],
-		packages=[nosetests, urllib.request],
-     )
+	version='0.1',
+	description='Org to Anki notes parser',
+	author='Conor OKelly',
+	author_email='okellyconor@gmail.com',
+	url='https://github.com/c-okelly/org_to_anki',
+	install_requires=['requests','nose'],
+	packages=['org_to_anki'],
+	entry_points={
+		'console_scripts': [
+			'org_to_anki = org_to_anki.main:main'
+		]
+	},
+	scripts=['bin/foo']
+	)
