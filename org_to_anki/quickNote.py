5e49afe66c2b01d0c94c7708a76730b4e239d77c
c-okelly <okellyconor@gmail.com>
2018-03-02 00:10:45 +0000

    Add new test for quickNotes

--- org_to_anki/quickNote.py
+++ org_to_anki/quickNote.py
@@ -3,28 +3,28 @@ from . import config
 
 
 def quickNote():
-
-    filePath = config.quickNotesOrgPath
-
     utils.createQuickNotesFile()
 
+    newQuestion = ""
+    question = input("Enter the question.\n").strip()
+    newQuestion += "* " + question + "\n"
+
+    answer = ""
+    while True:
+        answer = input(
+            "Enter an answer. Enter blank answer to exit.\n").strip()
+        if answer == "":
+            break
+        else:
+            newQuestion += "** " + answer + "\n"
+    
+    writeQuickNote(newQuestion)
+
+def writeQuickNote(question, filePath = None):
+    if filePath == None:
+        filePath = config.quickNotesOrgPath
     with open(filePath, "a") as orgFile:
-
-        newQuestion = ""
-        question = input("Enter the question.\n").strip()
-        newQuestion += "* " + question + "\n"
-
-        answer = ""
-        while True:
-            answer = input(
-                "Enter an answer. Enter blank answer to exit.\n").strip()
-            if answer == "":
-                break
-            else:
-                newQuestion += "** " + answer + "\n"
-
-        orgFile.write(newQuestion)
-
+        orgFile.write(question)
 
 if __name__ == "__main__":
     quickNote()
---
+++ tests/testQuickNote.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append('../org_to_anki')
+import os
+
+from org_to_anki.quickNote import writeQuickNote
+from org_to_anki.ankiClasses import AnkiQuestion
+from org_to_anki.utils import createQuickNotesFile
+
+def testQuickNoteWriter():
+    dir = os.path.dirname(__file__)
+    dirPath = dir + "/" + "orgNotes"
+    filePath = dirPath + "/" + "quickOrgNotes.org"
+    createQuickNotesFile(dirPath)
+
+    # assert(os.path.exists(dirPath) == True)
+    # Build question
+    q = "* What is the capital of Ireland?\n** Dublin\n"
+    writeQuickNote(q, filePath)
+    with open(filePath, "r") as orgFile:
+        content = orgFile.read()
+    
+    assert(q == content)
+    os.remove(filePath)
+    os.rmdir(dirPath)
+    
+
