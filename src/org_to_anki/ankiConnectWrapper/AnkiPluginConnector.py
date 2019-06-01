cdccda6285c380f3fbe7539be8bfe41556863623
Conor OKelly <okellyconor@gmail.com>
2019-06-01 13:15:37 +0100

    Allow defaultDeck for be overridden

--- src/org_to_anki/ankiConnectWrapper/AnkiPluginConnector.py
+++ src/org_to_anki/ankiConnectWrapper/AnkiPluginConnector.py
@@ -20,10 +20,10 @@ except:
 
 class AnkiPluginConnector:
 
-    def __init__(self):
+    def __init__(self, defaultDeck=config.defaultDeck):
         self.AnkiBridge = AnkiBridge()
-        self.defaultDeck = config.defaultDeck
-        self.AnkiNoteBuilder = AnkiNoteBuilder()
+        self.defaultDeck = defaultDeck
+        self.AnkiNoteBuilder = AnkiNoteBuilder(self.defaultDeck)
 
     def uploadNewDeck(self, deck): # AnkiDeck
 
